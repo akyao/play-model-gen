fd100347ec2507038952830e7518a52815f2e8fd
konuma <konuma_akio@cyberbuzz.co.jp>
2012-05-08 13:43:15 +0900

    java.sql.Date系ではなく、java.util.Dateを仕様するように修正した

--- src/play/modules/modelGen/ColumnForPlay.java
+++ src/play/modules/modelGen/ColumnForPlay.java
@@ -1,12 +1,18 @@
 package play.modules.modelGen;
 
 import java.sql.ResultSet;
+import java.sql.Types;
 import java.util.Set;
 
 import me.stormcat.maven.plugin.s2jdbcgen.Constants.ColumnMetaColumn;
+import me.stormcat.maven.plugin.s2jdbcgen.Constants.MappedType;
 import me.stormcat.maven.plugin.s2jdbcgen.meta.Column;
 
-
+/**
+ *
+ * @author konuma_akio
+ *
+ */
 public class ColumnForPlay extends Column{
 
     public ColumnForPlay(ResultSet resultSet, Set<String> primaryKeySet) {
@@ -17,13 +23,10 @@ public class ColumnForPlay extends Column{
     public String getColumnAnnotation() {
         StringBuilder builder = new StringBuilder("@Column(");
         //name
-        builder.append(String.format("name = \"%s\"", getFieldName()));
+        builder.append(String.format("name = \"%s\"", getColumnName()));
         // nullable
         builder.append(", nullable = ");
         builder.append(isNullable());
-        // unique
-        // insertable
-        // updatable
         // length
         if (isStringType()) {
             builder.append(", ");
@@ -34,14 +37,25 @@ public class ColumnForPlay extends Column{
             builder.append("precision = ");
             builder.append(getColumnSize());
         }
-
-        //name
-
-        //nullable
-
-        //length
+        // unique
+        // insertable
+        // updatable
 
         builder.append(")");
         return builder.toString();
+    }
+
+    @Override
+    public String getJavaType() {
+        switch (getDataType()) {
+            case Types.DATE:
+                return "java.util.Date";
+            case Types.TIMESTAMP:
+                return "java.util.Date";
+            case Types.TIME:
+                return "java.util.Date";
+            default:
+                return super.getJavaType();
+        }
     }
 }
